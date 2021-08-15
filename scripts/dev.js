263dd36073ff44f839da5080bd7b51a50d126aab
GavinLam164 <m13590491852@163.com>
2021-08-15 23:52:34 +0800

    speed-masure

--- scripts/dev.js
+++ scripts/dev.js
@@ -13,14 +13,9 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler))
 
-app.use(require('connect-history-api-fallback')())
+app.use('/', require('connect-history-api-fallback')())
 
-// 通常用于加载静态资源
 app.use(express.static(path.resolve(__dirname, '../dist')))
-
-app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
-})
 
 compiler.run(() => {
 	app.listen(3001, (error) => {
@@ -31,5 +26,4 @@ compiler.run(() => {
 		}
 		console.log('listen 3001')
 	})
-
 })
